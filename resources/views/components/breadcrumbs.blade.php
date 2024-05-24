1473f232f8230dbff8234f97e82374b914e3f820
Dangelo Brusorio <ddangelo.brusorio@gmail.com>
2024-05-24 15:25:59 +0200

    style: breadcrumbs icon should be: >

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -12,7 +12,7 @@
             {{-- Full path in breadcrumbs --}}
             @foreach($this->breadcrumbs as $key => $breadcrumb)
                 <li class="fi-breadcrumbs-item flex gap-x-2">
-                    <x-filament::icon icon="heroicon-o-chevron-left" class="h-4 w-4 m-auto text-gray-400 dark:text-gray-500"/>
+                    <x-filament::icon icon="heroicon-o-chevron-right" class="h-4 w-4 m-auto text-gray-400 dark:text-gray-500"/>
 
                     <a href="#" wire:click="openPath('{{ $key }}')" class="fi-breadcrumbs-item-label text-sm font-medium text-gray-500 transition duration-75 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
                         {{ $breadcrumb }}
