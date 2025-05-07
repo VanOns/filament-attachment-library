ee3d117f57ef07a7f503eaacf84023a0cc44ca4d
Danique Wijnalda <danique@van-ons.nl>
2025-05-07 16:28:24 +0200

    fix(FML-82): handle disabled state

--- resources/views/forms/components/attachment-field.blade.php
+++ resources/views/forms/components/attachment-field.blade.php
@@ -1,12 +1,29 @@
 <x-dynamic-component
-        x-data="{ state: $wire.$entangle('{{ $getStatePath() }}').live }"
-        x-on:selected-attachments-updated.window="$event.detail.statePath === '{{$getStatePath()}}' ? $wire.$set('{{$getStatePath()}}', $event.detail.attachments) : ''"
-        x-init="if($store.attachmentBrowser !== undefined){ $store.attachmentBrowser.addStatePath('{{$getStatePath()}}', {state: state ?? [], multiple: '{{$getMultiple()}}', showActions: true, showMime: false, mime: '{{$getMime()}}'}) }"
+    x-data="{ state: $wire.$entangle('{{ $getStatePath() }}').live }"
+    x-on:selected-attachments-updated.window="$event.detail.statePath === '{{ $getStatePath() }}' ? $wire.$set('{{ $getStatePath() }}', $event.detail.attachments) : ''"
+    x-init="if ($store.attachmentBrowser !== undefined) {
+        $store.attachmentBrowser.addStatePath('{{ $getStatePath() }}', {
+            state: state ?? [],
+            multiple: {{ $getMultiple() ? 'true' : 'false' }},
+            showActions: true,
+            showMime: false,
+            mime: '{{ $getMime() }}',
+            disabled: {{ $isDisabled() ? 'true' : 'false' }}
+        })
+    }"
 
-        {{-- Failover if attachmentBrowser isn't yet defined. --}}
-        x-on:attachment-browser-loaded-js.window="$store.attachmentBrowser.addStatePath('{{$getStatePath()}}', {state: state ?? [], multiple: '{{$getMultiple()}}', showActions: true, showMime: false, mime: '{{$getMime()}}'})"
-        :component="$getFieldWrapperView()"
-        :field="$field">
+    {{-- Failover if attachmentBrowser isn't yet defined. --}}
+    x-on:attachment-browser-loaded-js.window="$store.attachmentBrowser.addStatePath('{{ $getStatePath() }}', {
+        state: state ?? [],
+        multiple: {{ $getMultiple() ? 'true' : 'false' }},
+        showActions: true,
+        showMime: false,
+        mime: '{{ $getMime() }}',
+        disabled: {{ $isDisabled() ? 'true' : 'false' }}
+    })"
+    :component="$getFieldWrapperView()"
+    :field="$field"
+>
     <div>
 
         <div class="flex gap-2 flex-wrap">
@@ -15,15 +32,20 @@
                 :attachments="$getAttachments()"
                 :statePath="$getStatePath()"
                 :withContextMenu="false"
+                :disabled="$isDisabled()"
                 wire:key="attachment-item-list-{{ Str::random(10) }}"
             />
         </div>
 
         <x-filament::button
             x-on:click="$dispatch('open-modal', {id: 'attachment-modal', statePath: '{{ $getStatePath() }}'})"
-            x-on:selected-attachments-updated.window="$event.detail.statePath === '{{$getStatePath()}}' ? $wire.$set('{{$getStatePath()}}', $event.detail.attachments) : ''"
-            class="mt-2"
+            x-on:selected-attachments-updated.window="$event.detail.statePath === '{{ $getStatePath() }}' ? $wire.$set('{{ $getStatePath() }}', $event.detail.attachments) : ''"
+            x-bind:disabled="{{ $isDisabled() }}"
             icon="heroicon-o-document"
+            @class([
+                'mt-2',
+                'opacity-50 pointer-events-none' => $isDisabled(),
+            ])
         >
             {{ __('filament-attachment-library::views.field.pick') }}
         </x-filament::button>
--- resources/views/pages/attachments.blade.php
+++ resources/views/pages/attachments.blade.php
@@ -1,7 +1,12 @@
 <x-filament::page
-    x-on:attachment-browser-loaded-js.window.once="
-        $store.attachmentBrowser.setCurrentState('default', {state: null, multiple: false, showActions: true, showMime: true, statePath: 'default'})
-    "
+    x-on:attachment-browser-loaded-js.window.once="$store.attachmentBrowser.setCurrentState('default', {
+        state: null,
+        multiple: false,
+        showActions: true,
+        showMime: true,
+        statePath: 'default',
+        disabled: false
+    })"
 >
     <livewire:attachment-browser />
 
