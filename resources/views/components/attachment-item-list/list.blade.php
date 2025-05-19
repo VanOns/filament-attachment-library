14b2ef759b411473a8a5a016e1bd5fd7c40ced24
Danique Wijnalda <danique@van-ons.nl>
2025-05-19 11:12:25 +0200

    feat(FML-65): add thumbnail to list view

--- resources/views/components/attachment-item-list/list.blade.php
+++ resources/views/components/attachment-item-list/list.blade.php
@@ -9,19 +9,30 @@
         ? 'bg-black dark:bg-gray-300 dark:text-white text-white hover:dark:bg-red-400 hover:dark:text-white'
         : 'bg-white dark:bg-gray-{{ $inModal ? '800' : '900' }} hover:bg-gray-{{ $inModal ? '800' : '900' }} hover:dark:bg-gray-300 hover:text-white hover:dark:text-black'"
     @class([
-        'cursor-pointer relative flex flex-row items-center justify-between h-8 w-full px-4 py-6 transition ease-in-out box-border group',
+        'cursor-pointer relative flex flex-row items-center justify-between h-16 w-full px-4 py-6 transition ease-in-out box-border group',
         'rounded-xl shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10',
     ])
 >
-    {{-- Attachment item icon --}}
-    <div class="flex items-center gap-x-2">
-        <p x-text="attachment.name" class="max-w-48 overflow-hidden box line-clamp-1"></p>
+    <div class="flex items-center gap-x-3">
+        {{-- Preview image if attachment is image --}}
+        <template x-if="attachment.class === 'attachment' && attachment.is_image">
+            <img
+                alt="attachment.alt"
+                loading="lazy"
+                :src="attachment.thumbnail_url"
+                class="relative rounded-lg overflow-hidden h-12 w-12 object-center opacity-60 object-cover ring-1 ring-gray-950/10 dark:ring-white/10"
+            >
+        </template>
 
-        <p
-            class="block text-sm font-medium opacity-60"
-            x-show="attachment.class === 'attachment'"
-            x-text="attachment.size + ' MB'"
-        ></p>
+        <div class="flex flex-col">
+            <p x-text="attachment.name" class="max-w-48 overflow-hidden box line-clamp-1"></p>
+
+            <p
+                class="block text-sm font-medium opacity-60"
+                x-show="attachment.class === 'attachment'"
+                x-text="attachment.size + ' MB'"
+            ></p>
+        </div>
     </div>
 
     {{-- Attachment item actions & hover indicator --}}
