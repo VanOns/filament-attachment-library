ceb0957d8083d8bc9bedfce876fe454be51506a6
Jesse Alaerds <Jesse@van-ons.nl>
2026-06-12 11:26:16 +0200

    style(info): drop dark-mode opacity on preview media

--- resources/views/livewire/attachment-info.blade.php
+++ resources/views/livewire/attachment-info.blade.php
@@ -22,7 +22,7 @@
                         alt="{{ $attachment->alt }}"
                         loading="lazy"
                         src="{{ $attachment->thumbnailUrl() }}"
-                        class="relative rounded-lg dark:opacity-80 focus-within:ring-2 focus-within:ring-offset-4 focus-within:ring-offset-gray-100 focus-within:ring-primary-600 h-full w-auto max-h-48 m-auto"
+                        class="relative rounded-lg focus-within:ring-2 focus-within:ring-offset-4 focus-within:ring-offset-gray-100 focus-within:ring-primary-600 h-full w-auto max-h-48 m-auto"
                     >
                 @endif
 
@@ -30,7 +30,7 @@
                     <video
                         src="{{ $attachment->url }}"
                         controls
-                        class="relative object-cover object-center rounded-lg dark:opacity-80 focus-within:ring-2 focus-within:ring-offset-4 focus-within:ring-offset-gray-100 focus-within:ring-primary-600 h-full w-full max-h-48"
+                        class="relative object-cover object-center rounded-lg focus-within:ring-2 focus-within:ring-offset-4 focus-within:ring-offset-gray-100 focus-within:ring-primary-600 h-full w-full max-h-48"
                     ></video>
                 @endif
 
